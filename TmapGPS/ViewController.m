6edc1a76994a643854eba2e4dfd41b2c9813ff17
KiWoongJin <jkw87first@naver.com>
2014-01-17 14:26:32 +0900

    TmapGPS

--- TmapGPS/ViewController.m
+++ TmapGPS/ViewController.m
@@ -7,17 +7,55 @@
 //
 
 #import "ViewController.h"
+#import "TMapView.h"
 
-@interface ViewController ()
+#define APP_KEY @"9808e4e1-db20-3f1f-8b3c-77606b3b71a1"
+#define TOOLBAR_HIGHT 60
+
+@interface ViewController () <TMapGpsManagerDelegate>
+
+@property (strong, nonatomic) TMapGpsManager *gpsManager;
+@property (strong, nonatomic) TMapView *mapView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark GPS Manager's Delegate
+- (void)locationChanged:(TMapPoint *)newTmp{
+    NSLog(@"Location Changed : %@", newTmp);
+    [self.mapView setCenterPoint:newTmp];
+    
+}
+
+- (void)headingChanged:(double)heading{
+    
+}
+- (IBAction)switchGPS:(id)sender {
+    UISwitch *_switch = (UISwitch *)sender;
+    
+    if(YES == _switch.on){
+        [self.gpsManager openGps];
+    }
+    else{
+        [self.gpsManager closeGps];
+    }
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    CGRect rect = CGRectMake(0, TOOLBAR_HIGHT, self.view.frame.size.width, self.view.frame.size.height - TOOLBAR_HIGHT);
+    self.mapView = [[TMapView alloc]initWithFrame:rect];
+    [self.mapView setSKPMapApiKey:APP_KEY];
+    
+    self.mapView.zoomLevel =12.0;
+    [self.view addSubview:self.mapView];
+    
+    self.gpsManager = [[TMapGpsManager alloc]init];
+    [self.gpsManager setDelegate:self];
 }
 
 - (void)didReceiveMemoryWarning
